8ad91ed78f700c09302e702584703e5ba4904162
Manuel Hesselbarth <jumper85@email.de>
2019-12-01 16:39:16 +0100

    Spinner component

--- js/bundles/messages/components/Messages.svelte
+++ js/bundles/messages/components/Messages.svelte
@@ -1,11 +1,57 @@
-<script></script>
+<script>
+    import Spinner from '../../../compontents/ui/Spinner.svelte';
+
+    let messages = [];
+    let loading = true;
+
+    const fetchMessages = async () => {
+        try {
+            const response = await fetch('https://my-json-server.typicode.com/next-direction/demo-data/messages');
+            messages = await response.json();
+            loading = false;
+        } catch(e) {
+            alert(`Fetching messages failed due to the following reason: ${e.message}`);
+        }
+    };
+
+    fetchMessages();
+</script>
 
 <style>
+    * {
+        font-family: sans-serif;
+    }
+
     h1 {
         font-family: sans-serif;
         font-size: 2rem;
         color: green;
     }
+
+    h3 {
+        background: lightgrey;
+        padding: 0.4rem;
+        margin-bottom: 0;
+    }
+
+    p {
+        margin-top: 0;
+        padding: 0.4rem;
+        border-width: 0 1px 1px 1px;
+        border-color: lightgrey;
+        border-style: solid;
+    }
 </style>
 
 <h1>Messages</h1>
+
+{#if loading}
+    <Spinner color="grey" size="10rem"/>
+{:else}
+    {#each messages as message (message.id)}
+        <h3>Created at {message.date}</h3>
+        <p>
+            {message.message}
+        </p>
+    {/each}
+{/if}
---
+++ js/compontents/ui/Spinner.svelte
@@ -0,0 +1,68 @@
+<script>
+    export let color = '#FFFFFF';
+    export let size = '20px';
+</script>
+
+<style>
+    .lds-ellipsis {
+        margin: auto;
+        position: relative;
+        width: 4em;
+        height: 1em;
+    }
+    .lds-ellipsis div {
+        position: absolute;
+        top: 0.2em;
+        width: 0.6em;
+        height: 0.6em;
+        border-radius: 50%;
+        animation-timing-function: cubic-bezier(0, 1, 1, 0);
+    }
+    .lds-ellipsis div:nth-child(1) {
+        left: 0.4em;
+        animation: lds-ellipsis1 0.6s infinite;
+    }
+    .lds-ellipsis div:nth-child(2) {
+        left: 0.4em;
+        animation: lds-ellipsis2 0.6s infinite;
+    }
+    .lds-ellipsis div:nth-child(3) {
+        left: 1.6em;
+        animation: lds-ellipsis2 0.6s infinite;
+    }
+    .lds-ellipsis div:nth-child(4) {
+        left: 2.8em;
+        animation: lds-ellipsis3 0.6s infinite;
+    }
+    @keyframes lds-ellipsis1 {
+        0% {
+            transform: scale(0);
+        }
+        100% {
+            transform: scale(1);
+        }
+    }
+    @keyframes lds-ellipsis3 {
+        0% {
+            transform: scale(1);
+        }
+        100% {
+            transform: scale(0);
+        }
+    }
+    @keyframes lds-ellipsis2 {
+        0% {
+            transform: translate(0, 0);
+        }
+        100% {
+            transform: translate(200%, 0);
+        }
+    }
+</style>
+
+<div class="lds-ellipsis" style="font-size: {size}">
+    <div style="background: {color}"></div>
+    <div style="background: {color}"></div>
+    <div style="background: {color}"></div>
+    <div style="background: {color}"></div>
+</div>
